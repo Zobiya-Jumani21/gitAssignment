6ad02df6aa108400f4df1eaf46813f083e564dbe
Zobiya Jumani <106553015+Zobiya-Jumani21@users.noreply.github.com>
2024-03-03 11:37:12 +0500

    creat login and register form and design the form also make responsive

--- js/script.js
+++ js/script.js
@@ -2,3 +2,35 @@ let navbar = document.querySelector('.header .navbar')
 document.querySelector('#menu-btn').oneclick = () =>{
 navbar.classList.add('active');
 }
+
+document.querySelector('#close-navbar').oneclick = () =>{
+    navbar.classList.remove('active');
+    };
+
+    let registerBtn = document.querySelector('.account-form .register-btn');
+    let loginBtn = document.querySelector('.account-form .login-btn');
+
+    registerBtn.oneclick = () =>{
+        registerBtn.classList.add('active');
+        loginBtn.classList.remove('active');
+        document.querySelector('.account-form .login-form').classList.remove('active');
+        document.querySelector('.account-form .register-form').classList.add('active');
+    };
+
+    loginBtn.oneclick = () =>{
+        registerBtn.classList.remove('active');
+        loginBtn.classList.add('active');
+        document.querySelector('.account-form .login-form').classList.add('active');
+        document.querySelector('.account-form .register-form').classList.remove('active');
+    };
+
+    let accountForm = document.querySelector('.account-form')
+document.querySelector('#account-btn').oneclick = () =>{
+accountForm.classList.add('active');
+};
+
+document.querySelector('#close-form').oneclick = () =>{
+    accountForm.classList.remove('active');
+    };
+
+
