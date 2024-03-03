89c671b289e6c8998f183b4758000e2791d7fd82
Mehak <mehaksindhi9@gmail.com>
2024-03-03 05:04:19 +0500

    course page completed

--- js/script.js
+++ js/script.js
@@ -2,3 +2,10 @@ let navbar = document.querySelector('.header .navbar')
 document.querySelector('#menu-btn').oneclick = () =>{
 navbar.classList.add('active');
 }
+document.querySelector('.load-more .btn').oneclick = ()=>{
+    document.querySelectorAll('.courses .box-container .hide').forEach(show=>{
+        show.style.display='black';
+
+    });
+    document.querySelector('.load-more .btn').style.display='none'
+};
