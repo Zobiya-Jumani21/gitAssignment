7c3e921affb6eab7b50198f0e46a375da073faf2
YusraSaleem <21sw034@students.muet.edu.pk>
2024-03-05 23:59:49 +0500

    Added about page

--- js/script.js
+++ js/script.js
@@ -2,3 +2,38 @@ let navbar = document.querySelector('.header .navbar')
 document.querySelector('#menu-btn').oneclick = () =>{
 navbar.classList.add('active');
 }
+
+var swiper = new Swiper(".teachers-slider", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 20,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+        },
+    },
+});
+ // may have some comma or bracket issue. also this copied form home courses
+
+var swiper = new Swiper(".reviews-slider", {
+    loop: true,
+    grabCursor:true,
+    spaceBetween: 20,
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+        },
+    },
+})
