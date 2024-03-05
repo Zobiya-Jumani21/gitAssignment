6b7b85f638c5462f76d61510756c7c72e786c853
Zobiya Jumani <106553015+Zobiya-Jumani21@users.noreply.github.com>
2024-03-05 23:18:49 +0500

    creat footer of the education website also resolved bugs and created some tabs include information on those tabs also design those and make responsive

--- js/script.js
+++ js/script.js
@@ -1,36 +1,64 @@
-let navbar = document.querySelector('.header .navbar')
-document.querySelector('#menu-btn').oneclick = () =>{
+let navbar = document.querySelector('.header .navbar') 
+document.querySelector('#menu-btn').onclick = () =>{
 navbar.classList.add('active');
 }
 
-document.querySelector('#close-navbar').oneclick = () =>{
+document.querySelector('#close-navbar').onclick = () =>{
     navbar.classList.remove('active');
     };
 
     let registerBtn = document.querySelector('.account-form .register-btn');
     let loginBtn = document.querySelector('.account-form .login-btn');
 
-    registerBtn.oneclick = () =>{
+    registerBtn.onclick = () =>{
         registerBtn.classList.add('active');
         loginBtn.classList.remove('active');
         document.querySelector('.account-form .login-form').classList.remove('active');
         document.querySelector('.account-form .register-form').classList.add('active');
     };
 
-    loginBtn.oneclick = () =>{
+    loginBtn.onclick = () =>{
         registerBtn.classList.remove('active');
         loginBtn.classList.add('active');
         document.querySelector('.account-form .login-form').classList.add('active');
-        document.querySelector('.account-form .register-form').classList.remove('active');
+        document.querySelector('.account-form .register-btn').classList.remove('active');
     };
 
     let accountForm = document.querySelector('.account-form')
-document.querySelector('#account-btn').oneclick = () =>{
+document.querySelector('#account-form').onclick = () =>{
 accountForm.classList.add('active');
 };
 
-document.querySelector('#close-form').oneclick = () =>{
+document.querySelector('#close-form').onclick = () =>{
     accountForm.classList.remove('active');
     };
 
+
+    var swiper = new Swiper(".home-slider", {
+        pagination: {
+          el: ".swiper-pagination",
+          clickabke:true,
+        },
+        loop:true,
+        grabCursor:true,
+      });
+
+      var swiper = new Swiper(".home-courses-slider", {
+       
+        loop:true,
+        grabCursor:true,
+        spaceBetween: 20,
+        breakpoints: {
+            0: {
+              slidesPerView: 1,
+            },
+            768: {
+              slidesPerView: 2,
+            },
+            991: {
+              slidesPerView: 3,
+              
+            },
+          },
+      });
 
